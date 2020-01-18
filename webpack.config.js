0f5e45e021c2631af65d5567d8d35967288cfa66
benishouga <benishouga@gmail.com>
2020-01-18 19:42:10 +0900

    Added docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    filename: 'index.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'docs')
   },
 
